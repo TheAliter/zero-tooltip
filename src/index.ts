4c9751e0b7af8fec9fb2fcf13828b728737f40b4
The Tester <test@test.lv>
2023-10-08 18:33:55 +0300

    fixed missing type export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import ZeroTooltip from "./tooltip"
 import TooltipConfig from "./types/tooltipConfig"
+import TooltipLocalConfig from "./types/tooltipLocalConfig"
 import TooltipPosition from "./types/tooltipPosition"
 import TooltipPositions from "./types/tooltipPositions"
 
 export default ZeroTooltip
-export type { TooltipConfig as ZeroTooltipConfig, TooltipPosition as ZeroTooltipPosition, TooltipPositions as ZeroTooltipPositions }
+
+export type { 
+    TooltipConfig as ZeroTooltipConfig, 
+    TooltipPosition as ZeroTooltipPosition, 
+    TooltipPositions as ZeroTooltipPositions, 
+    TooltipLocalConfig as ZeroTooltipLocalConfig 
+}
