0c512ecb6e41aea7b0e5a8fdf40d882f70c7a7ec
Andris P <andris.paskovskis@arkbauer.com>
2024-11-27 16:16:39 +0200

    test: update testing ground and package version

--- demo/App.vue
+++ demo/App.vue
@@ -9,23 +9,57 @@
 
         Change setting
     </button>
+
+    <br>
+    <br>
+
+    <button type="button" @click="toggle" >open</button>   
+
+    <OverlayPanel ref="op">
+            <button style="padding: 50px; background: greenyellow;" v-tooltip="tooltipConfig">Test button</button>
+    </OverlayPanel>
   </div>
 </template>
 
 <script setup lang="ts">
   import { reactive } from 'vue'
   import TooltipLocalConfig from '../src/types/tooltipLocalConfig'
+  import OverlayPanel from 'primevue/overlaypanel';
+
 
   const tooltipConfig: TooltipLocalConfig = reactive({ 
     content: 'Tooltip testers form',
-    offsetFromSource: 10,
+    showDelay: 200,
+    hideDelay: 10,
+    defaultPosition: 'bottom',
   })
+
+  import { ref } from "vue";
+
+  const members = ref([
+    { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png', email: 'bernardo@email.com', role: 'Editor' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png', email: 'ioni@email.com', role: 'Viewer' }
+]);
+const op = ref();
+
+const toggle = (event) => {
+    op.value.toggle(event);
+}
 </script>
 
 <style>
 p {
   margin: 0;
 }
+
+.p-overlaypanel-enter-active {
+    transition: transform 1s cubic-bezier(0, 0, 0.2, 1), opacity 1s cubic-bezier(0, 0, 0.2, 1);
+  }
+
+  .p-overlaypanel-leave-active {
+    transition: opacity 1 linear;
+  }
 </style>
 
 
--- demo/main.ts
+++ demo/main.ts
@@ -2,11 +2,15 @@ import '../src/style.css'
 import { createApp } from 'vue'
 import MyApp from './App.vue'
 import ZeroTooltip from '../src/index'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/aura-light-green/theme.css";
 
 const app = createApp(MyApp)
 
 app.use(ZeroTooltip, {
 })
+
+app.use(PrimeVue, { /* options */ });
 
 app.mount('#app')
 
