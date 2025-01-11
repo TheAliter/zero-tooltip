4df74a61cce7be6ae8f70305eea8929ed1c6365b
TheAliter <andris.paskovskis@inbox.lv>
2025-01-11 08:05:31 +0200

    feat: created first edition of proper testing playground

--- demo/App.vue
+++ demo/App.vue
@@ -1,83 +1,151 @@
-
 <template>
-  <div>
-    <h1>Playground</h1>
+    <div>
+        <h1 style="text-align: center;">Playground</h1>
 
-    <button 
-        v-tooltip="tooltipConfig" 
-        style="position: relative; padding: 10px;">
+        <!-- TOP-LEFT button -->
+        <button v-tooltip="{
+            content: 'Bottom right',
+            defaultPosition: 'top'
+        }" style="position: fixed; left: 0; top: 0;">
 
-        left block
-    </button>
+            Trying top
+        </button>
 
-    <button 
-        v-tooltip="tooltipConfig2" 
-        @click="toggle"
-        style="position: relative; padding: 10px;">
+        <!-- TOP-RIGHT button -->
+        <button v-tooltip="{
+            content: 'Bottom left',
+            defaultPosition: 'right'
+        }" style="position: fixed; right: 0; top: 0;">
 
-        Right block
-    </button>
+            Trying right
+        </button>
 
-    <br>
-    <br>
+        <!-- BOTTOM-LEFT button -->
+        <button v-tooltip="{
+            content: 'Top right',
+            defaultPosition: 'left'
+        }" style="position: fixed; left: 0; bottom: 0;">
 
-    <button type="button" @click="toggle" >open</button>   
+            Trying left
+        </button>
 
-    <OverlayPanel ref="op">
-            <button style="padding: 50px; background: greenyellow;" v-tooltip="tooltipConfig">Test button</button>
-    </OverlayPanel>
+        <!-- BOTTOM-RIGHT button -->
+        <button v-tooltip="{
+            content: 'Top left',
+            defaultPosition: 'bottom'
+        }" style="position: fixed; right: 0; bottom: 0;">
 
-    <div style="height: 1200px; width: 20px; background: red;"></div>
-  </div>
+            Trying bottom
+        </button>
+
+
+        <!-- Overlay Panel behavior test -->
+        <h2 style="margin-bottom: 0;">Test z-index</h2>
+        <h2 style="margin-top: 0;">Test position for tooltip with target in overlay (which enters with transition)</h2>
+        <button type="button" @click="toggle">
+            Open Overlay panel
+        </button>
+
+        <OverlayPanel ref="overlayPanelElement">
+            <button 
+                style="padding: 10px; background: greenyellow;" 
+                v-tooltip="{
+                    content: 'Default tooltip config',
+                    zIndex: 1002
+                }">
+
+                Test default tooltip behavior
+            </button>
+        </OverlayPanel>
+
+        <div style="margin-top: 60px;">
+            <h2 style="margin-block: 0;">Test hide/show delay </h2>
+            <h2 style="margin-block: 0;">Test show multiple tooltip at the same time</h2>
+            <h2 style="margin-top: 0;">Test select tooltip text for hide delay</h2>
+
+            <button 
+                v-tooltip="{
+                    content: 'First tooltip',
+                    hideDelay: 1000
+                }" 
+                style="">
+
+                Hide delay
+            </button>
+
+            <button 
+                v-tooltip="{
+                    content: 'Second tooltip',
+                    showDelay: 500,
+                }" 
+                style="margin-left: 8px;">
+
+                Show delay
+            </button>
+        </div>
+
+        <div style="margin-top: 60px;">
+            <h2>Test always show (UNDER CONSTRUCTION)</h2>
+
+            <button 
+                v-tooltip="{
+                    content: 'I am always visible',
+                }" 
+                style="">
+
+                Always show
+            </button>
+        </div>
+
+        <div style="margin-top: 60px; border: 1px solid red; height: 500px;">
+            <h2>Test hide on scroll</h2>
+
+            <button 
+                v-tooltip="{
+                    content: 'Hide on scroll',
+                }" 
+                style="">
+
+                Hide on scroll
+            </button>
+
+            <button 
+                v-tooltip="{
+                    content: 'Hide on scroll',
+                    showDelay: 500,
+                }" 
+                style="margin-left: 20px;">
+
+                Hide on scroll (with show delay)
+            </button>
+
+            <button 
+                v-tooltip="{
+                    content: 'Hide on scroll',
+                    hideDelay: 1000,
+                }" 
+                style="margin-left: 20px;">
+
+                Hide on scroll (with hide delay)
+            </button>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-  import { reactive } from 'vue'
-  import TooltipLocalConfig from '../src/types/tooltipLocalConfig'
-  import OverlayPanel from 'primevue/overlaypanel';
+    import { ref } from "vue";
+    import OverlayPanel from 'primevue/overlaypanel';
 
+    const overlayPanelElement = ref();
 
-  const tooltipConfig: TooltipLocalConfig = reactive({ 
-    content: 'Tooltip testers form',
-    showDelay: 0,
-    hideDelay: 1000,
-    defaultPosition: 'bottom',
-  })
-
-  const tooltipConfig2: TooltipLocalConfig = reactive({ 
-    content: 'Top tooltip',
-    showDelay: 0,
-    hideDelay: 0,
-    defaultPosition: 'top',
-  })
-
-  import { ref } from "vue";
-
-  const members = ref([
-    { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
-    { name: 'Bernardo Dominic', image: 'bernardodominic.png', email: 'bernardo@email.com', role: 'Editor' },
-    { name: 'Ioni Bowcher', image: 'ionibowcher.png', email: 'ioni@email.com', role: 'Viewer' }
-]);
-const op = ref();
-
-const toggle = (event) => {
-    tooltipConfig2.defaultPosition = 'right';
-}
+    const toggle = (event) => {
+        overlayPanelElement.value.toggle(event);
+    }
 </script>
 
 <style>
-p {
-  margin: 0;
-}
-
-.p-overlaypanel-enter-active {
-    transition: transform 1s cubic-bezier(0, 0, 0.2, 1), opacity 1s cubic-bezier(0, 0, 0.2, 1);
-  }
-
-  .p-overlaypanel-leave-active {
-    transition: opacity 1 linear;
-  }
+    p {
+        margin: 0;
+        padding: 0;
+    }
 </style>
-
-
-
