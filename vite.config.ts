e3df3599ce3d4921c5910cbb52a1612d7c556b38
TheAliter <andris.paskovskis@inbox.lv>
2025-01-11 08:51:40 +0200

    dev: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,27 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({ rollupTypes: true})],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ZeroTooltip',
-      fileName: 'zero-tooltip'
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
-      },
     },
-  },
+
+    plugins: [vue(), dts({ rollupTypes: true })],
+    
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'ZeroTooltip',
+            fileName: 'zero-tooltip'
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+    },
 })
