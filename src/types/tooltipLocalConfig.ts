06bb307a9438861add4ff6a3a7019ac80ce0e23b
Martins Zeltins <martins.zeltins@arkbauer.com>
2025-07-03 09:42:48 +0300

    [feat-tooltip-config-comments] chore: fix formatting

--- src/types/tooltipLocalConfig.ts
+++ src/types/tooltipLocalConfig.ts
@@ -5,9 +5,8 @@ type TooltipLocalConfig = {
      * REQUIRED. Tooltip text.
      * Text is rendered as HTML, thus it's possible to give simple HTML structure, e.g., <h1>Tooltip text</h1>
      */
-
     content: string,
-    
+
     /**
      * Define whether tooltip should be shown on hover (enabled/disabled).
      */
