33b7d76dd7ec6e7ebb6f1ff55de9c409d0a0d081
Martins Zeltins <martins.zeltins@arkbauer.com>
2025-07-03 09:38:50 +0300

    feat: add comments for tooltip config properties

--- src/types/tooltipLocalConfig.ts
+++ src/types/tooltipLocalConfig.ts
@@ -1,8 +1,20 @@
 import TooltipConfig from "./tooltipConfig"
 
 type TooltipLocalConfig = {
+    /**
+     * REQUIRED. Tooltip text.
+     * Text is rendered as HTML, thus it's possible to give simple HTML structure, e.g., <h1>Tooltip text</h1>
+     */
+
     content: string,
+    /**
+     * Define whether tooltip should be shown on hover (enabled/disabled).
+     */
     show?: boolean,
+
+    /**
+     * Define whether to show tooltip (on mount) without needing hover trigger.
+     */
     alwaysOn?: boolean,
 } & TooltipConfig
 
