df0996e7a92287638bc705357868c7ea7940dd59
Andris P <andris.paskovskis@arkbauer.com>
2025-07-10 15:27:44 +0300

    chore: added comments for config properties

--- src/types/tooltipConfig.ts
+++ src/types/tooltipConfig.ts
@@ -2,22 +2,89 @@ import TooltipPosition from "./tooltipPosition"
 import TooltipPositions from "./tooltipPositions"
 
 type TooltipConfig = {
+    /**
+     * A valid CSS query selector to specify where Tooltip gets appended.
+     */
     appendTo?: string,
+
+    /**
+     * Position of tooltip component relative to element that is being hovered.
+     */
     defaultPosition?: TooltipPosition,
+
+    /**
+     * Ordered list of fallback positions in case tooltip does not have enough space in default position. If none of given positions will have enough space for tooltip, then it will not be rendered.
+     */
     positions?: Partial<TooltipPositions>,
+
+    /**
+     * Tooltip offset in px from element that's being hovered *(arrow size is not added to this value)*
+     */
     offsetFromSource?: number,
+
+    /**
+     * Minimal allowed tooltip offset in `px` from viewport sides
+     */
     offsetFromViewport?: number,
+
+    /**
+     * Minimal tooltip width in `px` that will be allowed to render
+     */
     minWidth?: number,
+
+    /**
+     * Maximal tooltip width in `px` that will be allowed to render
+     */
     maxWidth?: number,
+
+    /**
+     * Tooltip container border width in `px`
+     */
     tooltipBorderWidth?: number,
+
+    /**
+     * List of classes that will be added to tooltip element
+     */
     tooltipClasses?: string,
+
+    /**
+     * List of classes that will be added to text element
+     */
     textClasses?: string,
+
+    /**
+     * Length of arrow hypotenuse in `px` (arrow is generated using border width property, creating square which gets divided in four triangles, thus `arrowSize` is length of square side)
+     */
     arrowSize?: number,
+
+    /**
+     * List of classes that will be added to arrow element
+     */
     arrowClasses?: string,
+
+    /**
+     * Minimal allowed arrow offset in `px` from tooltip corner. Used in situations when tooltip does not have enough space to be centered relative to element that is being hover, thus arrow is rendered closer to one of the tooltip corners
+     */
     arrowMinOffsetFromTooltipCorner?: number,
+
+    /**
+     * `z-index` css property value of tooltip
+     */
     zIndex?: number | string,
+
+    /**
+     * Delay in milliseconds after which to show tooltip while hovering over element
+     */
     showDelay?: number,
+
+    /**
+     * Delay in milliseconds after which to hide tooltip when leaving element boundaries
+     */
     hideDelay?: number,
+
+    /**
+     * Whether to show warning about empty tooltip text value in cases when tooltip was expected to be shown
+     */
     showWarnings?: boolean,
 }
 
--- src/types/tooltipLocalConfig.ts
+++ src/types/tooltipLocalConfig.ts
@@ -1,8 +1,20 @@
 import TooltipConfig from "./tooltipConfig"
 
 type TooltipLocalConfig = {
+    /**
+     * Tooltip text. Text is rendered as HTML, thus it's possible to give simple HTML structure, e.g., `<h1>Tooltip text</h1>`
+     */
     content: string,
+
+    /**
+     * Define whether tooltip should be shown on hover (enabled/disabled). Default value is `false` if `alwaysOn` is set, otherwise it's `true`.
+     */
     show?: boolean,
+
+    /**
+     * Define whether to show tooltip (on mount) without needing hover trigger
+     * If `alwaysOn` is set, `show` will be set to `false` by default.
+     */
     alwaysOn?: boolean,
 } & TooltipConfig
 
